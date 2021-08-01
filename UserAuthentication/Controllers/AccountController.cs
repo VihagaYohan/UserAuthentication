d77d99367ec4d1cc14a82b74d02625891d3fee82
VihagaYohan <vihagayohan94@gmail.com>
2021-08-01 08:38:44 +0530

    Added user register

---
+++ UserAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserAuthentication.ViewModel;
+
+namespace UserAuthentication.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+		private readonly UserManager<IdentityUser> userManager;
+		private readonly SignInManager<IdentityUser> signInManger;
+
+		public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManger)
+		{
+			this.userManager = userManager;
+			this.signInManger = signInManger;
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult> Register(RegisterVM model) 
+		{
+			var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+			var result = await userManager.CreateAsync(user, model.Password);
+			Console.WriteLine(result);
+
+			await signInManger.SignInAsync(user, isPersistent: false);
+			return Ok(result);
+		}
+	}
+}
